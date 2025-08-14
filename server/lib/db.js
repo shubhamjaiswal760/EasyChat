87af62e212c591220d72b8ee47d37e56e0ddf796
Shubahm760 <shubhamjais760@gmail.com>
2025-08-14 09:21:43 +0530

    mongo

--- server/lib/db.js
+++ server/lib/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 export const connectDB = async () =>{
     try {
         mongoose.connection.on('connected', ()=> console.log('Database Connected'));
-       await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`,{
+       await mongoose.connect(process.env.MONGODB_URI,{
         dbName: "chat_app_deployment",
        }) 
     } catch (error) {
